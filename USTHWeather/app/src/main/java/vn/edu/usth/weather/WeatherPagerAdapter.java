63e344de81f00d4eb7adf362e4109e15f9ff3d24
minoxd <minophen.aceta114@gmail.com>
2024-09-11 15:39:33 +0700

    remove redundant switch case

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherPagerAdapter.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherPagerAdapter.java
@@ -17,10 +17,6 @@ public class WeatherPagerAdapter extends FragmentPagerAdapter {
     @Override
     public Fragment getItem(int position) {
         switch (position) {
-            case 0:
-            case 1:
-            case 2:
-                return new WeatherAndForecastFragment();
             default: return new WeatherAndForecastFragment();
         }
     }
